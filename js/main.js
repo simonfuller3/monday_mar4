034bc453abde24924f6a2e0f4008300683380c9c
Fuller <s_fuller3@tss-hr420-dm16.fc.ca>
2019-03-04 15:54:22 -0500

    big nasty

--- js/main.js
+++ js/main.js
@@ -9,10 +9,26 @@
 			// debugging / error handling
 			// if we dont have a matching audio eement then kill the function
 			if (!audio) { return; } // return stops code execution
+			
+
 			console.log(audio);
 			audio.currentTime = 0;
 			audio.play();
+
+			let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+			key.classList.add('playing');
+	
+
 	}
+
+
+	function removePlayingClass(event) {
+		debugger;
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitioned", removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 
